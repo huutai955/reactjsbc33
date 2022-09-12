e5b0d0410b4f7037d361c05decb17dc68520c66e
HuuTai <tainguyen201507021@gmail.com>
2022-09-12 17:29:03 +0700

    Buoi1

---
+++ src/Components/DemoFuncCom.jsx
@@ -0,0 +1,18 @@
+// React class components: rcc
+import React, { Component } from 'react'
+
+
+
+export default class DemoFuncCom extends Component {
+
+    // Phương thức render chứa giao diện của thẻ <DemoFuncCom/>
+    render() {
+
+        // Nội dung giao diện sẻ chứa trong lệnh return của render
+        return (
+            <div>
+                <a href='#'>Header Components!!!</a>
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import DemoFuncCom from './Components/DemoFuncCom';
+import DemoHeader from './Components/DemoHeader';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div className='abc'>
+    <DemoFuncCom />
+    <DemoFuncCom />
+    <DemoFuncCom />
+    <DemoFuncCom />
+    <DemoFuncCom />
+    <DemoFuncCom />
+    <DemoHeader />
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
